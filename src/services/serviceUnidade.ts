049c89321a12ac96afa25bc4e6592f4763d9008a
Thyago Arraes <thyagosouzaarraes@gmail.com>
2023-06-01 12:01:45 -0300

    Update serviceUnidade.ts

--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,14 +1,61 @@
-import Curso from "../databases/models/curso"
+import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
+import { ReturnDocument } from "typeorm"
 
-const cursor = AppDataSource.getRepository(Curso)
+const cursor = AppDataSource.getRepository(Unidade)
 
-export class CreateCursoService {}
 
-export class ReadAllCursoService {}
+type newUnidadeRequest = {
+    descricao_unidade: string
+    carga_horaria_unidade: number
+    ordem : number
+    fk_curso: string
+}
 
-export class ReadOneCursoService {}
+export class CreateunidadeService {
+    async execute({
+        descricao_unidade,
+        carga_horaria_unidade,
+        ordem,
+        fk_curso,
+    }: newUnidadeRequest): Promise<Unidade | Error> {
 
-export class UpdateCursoService {}
+        if (await cursor.findOne({ where: { descricao_unidade } })) {
+        return new Error("unidade já cadastrado!")
+}
+
+const unidade = cursor.create({
+        descricao_unidade,
+        carga_horaria_unidade, 
+        ordem,
+        fk_curso,
+})
+
+    await cursor.save(unidade)
+
+    return unidade
+    }
+}
+
+
+export class ReadOneUnidadeService {
+    async execute({ descricao_unidade }: findOneUnidadeRequest): Promise<Unidade | Error> {
+    const unidade = await cursor.findOne({ where: { descricao_unidade } });
+
+    if (!unidade) {
+        return new Error("Unidade não encontrada!");
+    }}
+}
+
+export class UpdateUnidadeService {
+
+    async execute({ descricao_unidade }: findOneUnidadeRequest): Promise<Unidade | Error> {
+    const unidade = await cursor.findOne({ where: { descricao_unidade } });
+
+    if (!unidade) {
+    return new Error("Unidade não encontrada!");
+    }
+
+
 
 export class DeleteCursoService {}
